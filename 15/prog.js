feeb3db9fe9d9fa2a80fb04693d886ff78610823
sourabhlachyan <120714055+sourabhlachyan@users.noreply.github.com>
2023-08-06 00:22:18 +0530

    Add files via upload

---
+++ 15/prog.js
@@ -0,0 +1,12 @@
+
+var number1 = 5;
+var number2 = 10;
+var number3 = 15;
+var number4 = 20;
+var string = "helloworld";
+var booleanValue = true;
+
+
+console.log(number1,number2,number3,number4);
+console.log(string);
+console.log(booleanValue);
---
+++ 16/pro.js
@@ -0,0 +1,24 @@
+// Primitive Data Types
+let number = 5;                   
+let string = "Hello";      
+let boolean = true;                
+let nullValue = null;              
+let undefinedValue = undefined; 
+let bigNumberLiteral = 123n;   
+console.log(number,string,boolean,nullValue,undefinedValue,bigNumberLiteral);
+
+// Reference Data Types
+let array = [1, 2, 3,4,5];             
+let object = {                     
+  name: "girish",
+  age: 24,
+  mail: "test@gmail.com"
+  
+};
+console.log(array,object);
+let date = new Date(); 
+console.log(date);            
+let functionValue = function() {   
+  console.log("Hello!");
+};
+
